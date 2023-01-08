cb5debedcffb84ff08e00e08ae180da88c2aceb4
Kaosbeing <matheorigami@gmail.com>
2023-01-08 18:48:11 +0100

    config menu for the nodes

--- js/config.js
+++ js/config.js
@@ -2,6 +2,7 @@ export var canvas = document.querySelector("canvas");
 export var body = document.querySelector("body");
 export let ctx = canvas.getContext("2d");
 export let savebtn = document.querySelector("#saveButton");
+export let nodeMenu = document.querySelector("#node_config_menu");
 
 /* Size canvas to window size */
 canvas.width = window.visualViewport.width;
